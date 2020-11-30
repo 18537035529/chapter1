941fe968f4d9c667a29a69df1da5f975d978d5b8
wangyongkang <18537035529@163.com>
2020-11-30 11:43:22 +0800

    提交至本地仓库

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,23 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected  void doGet(HttpServletRequest request , HttpServletResponse reponse) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        System.out.println("当前时间==============================："+currentTime);
+        request.setAttribute("currentTime",currentTime);
+        request.getRequestDispatcher("/jsp/hello.jsp").forward(request,reponse);
+    }
+}
